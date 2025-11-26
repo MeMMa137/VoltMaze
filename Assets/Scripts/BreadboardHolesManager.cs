c9236405197b3e7aa48850d6ebf5a9d0a586bef5
Memma <132008224+MeMMa137@users.noreply.github.com>
2025-11-25 21:42:36 -0500

    Add reactive holes

--- Assets/Scripts/BreadboardHolesManager.cs
+++ Assets/Scripts/BreadboardHolesManager.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class BreadboardHolesManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [Header("Emptry obj w holes")]
+
+    public List<Transform> rowParents;
+
+    [Header("Higlight")] 
+    public Color highlightColor = Color.yellow;
+    private List<SpriteRenderer> holes = new List<SpriteRenderer>();
+
     void Start()
     {
-        
+        holes.Clear();
+        foreach(Transform row in rowParents)
+        {
+            foreach(Transform hole in row)
+            {
+                SpriteRenderer sr = hole.GetComponent<SpriteRenderer>();
+                if (sr != null)
+                {
+                    holes.Add(sr);
+                }
+            }
+        }
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+      Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        foreach (SpriteRenderer hole in holes)
+        {
+            Collider2D col = hole.GetComponent<Collider2D>();
+            if (col != null && col.OverlapPoint(mousePos))
+            {
+                hole.color = highlightColor;
+            }
+            else
+            {
+                hole.color = Color.white;
+            }
+        }
     }
 }
